f87a41802e3181a45d7131d2382dc49eb509633b
David Schmitt <david@dasz.at>
2014-02-03 10:37:29 +0100

    take over the exim class completely

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # primary class to connect the various parts of the configuration
 class exiscan ($master = false) {
   $bool_master = any2bool($master)
-  include exim, exiscan::spamassassin
+  include exiscan::spamassassin
 
+  class { 'exim': package => 'exim4-daemon-heavy'; }
 }
